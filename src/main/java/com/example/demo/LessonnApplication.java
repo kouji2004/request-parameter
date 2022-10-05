246102ba5a2086b9c4251a7c37c8673a1e604cc8
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-10-06 00:02:36 +0900

    spring bootでrequestが返ってくる

---
+++ src/main/java/com/example/demo/LessonnApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonnApplication.class, args);
+	}
+
+}
